e89e6905ea08d59fe38c86ab9b4ba4459bb6427f
Rachhek Shrestha <rachhek@gmail.com>
2023-07-15 14:15:08 +0300

    add function for plotting annotated image

--- utils.py
+++ utils.py
@@ -16,3 +16,49 @@ def get_azure_env_var():
         env_vars[key] = value
 
     return env_vars
+
+
+def plot_annotated_image(
+    input_image_path, bounding_box, dpi=250, page_height=15, page_width=5
+):
+    import cv2
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    image = cv2.imread(input_image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    boundingBox = bounding_box
+    new_bounding_box_values = [val * dpi for val in boundingBox]
+    pts = np.array(new_bounding_box_values, np.int32).reshape((-1, 1, 2))
+    image = cv2.polylines(image, [pts], True, (255, 0, 0), 10)
+    plt.figure(figsize=(page_width, page_height))
+    plt.imshow(image)
+    plt.axis("off")
+    plt.show()
+
+
+def save_annotated_image(
+    input_image_path,
+    bounding_box,
+    output_path,
+    dpi=250,
+    page_height=5,
+    page_width=5,
+):
+    import cv2
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    print("input_image_path: ", input_image_path)
+    print("Saving annotated image to: ", output_path)
+
+    image = cv2.imread(input_image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    boundingBox = bounding_box
+    new_bounding_box_values = [val * dpi for val in boundingBox]
+    pts = np.array(new_bounding_box_values, np.int32).reshape((-1, 1, 2))
+    image = cv2.polylines(image, [pts], True, (255, 0, 0), 5)
+    plt.figure(figsize=(page_width, page_height))
+    plt.imshow(image)
+    plt.axis("off")
+    plt.savefig(output_path)
